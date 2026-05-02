1cc17df7acbff5a8a67e04b6fbcd95bd22249af2
Hamza Ali <Hamza2.ali@live.uwe.ac.uk>
2026-05-02 18:36:58 +0100

    Week 1 Task 3

---
+++ main.c
@@ -0,0 +1,11 @@
+/* Filename: main.c
+        * Author: [Hamza Ali]
+* Date: [2/5/26]
+* Description: Speed Converter for UFMFGT-15-1.
+*/
+#include <stdio.h>
+int main(void) {
+`
+    return 0; // Indicates the program finished successfully.
+}
+
