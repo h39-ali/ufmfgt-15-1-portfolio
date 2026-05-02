e6f59cdf1f45b71627b9087c62094a79e764aa51
Hamza Ali <Hamza2.ali@live.uwe.ac.uk>
2026-05-02 18:40:29 +0100

    Week 1 Task 3

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 /* Filename: main.c
         * Author: [Hamza Ali]
 * Date: [2/5/26]
-* Description: Speed Converter for UFMFGT-15-1.
+* Description: My first C program for UFMFGT-15-1.
 */
 #include <stdio.h>
 int main(void) {
-`
+// This line prints a message to the console.
+    printf("Hello, World! My name is Hamza Ali and my ID is 24062012.\n");
     return 0; // Indicates the program finished successfully.
 }
 
