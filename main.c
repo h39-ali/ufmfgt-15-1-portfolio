1ede89294ddc88c612f49b1b6d7b3cc562cdfcb9
Hamza Ali <Hamza2.ali@live.uwe.ac.uk>
2026-05-03 20:01:48 +0100

    Week 3 task1

--- main.c
+++ main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
-#include <math.h>
 int main() {
-    float height, length;
-    printf("Enter height of Right angle triangle");
-    scanf("%f", &height);
-    printf("Enter length of Right angle triangle");
-    scanf("%f", &length);
-    float hypotenuse = sqrt(pow(height, 2) + pow(length, 2));
-    float perimeter = height + length + hypotenuse;
-    printf("Hypotenuse is: %f", hypotenuse);
-    printf("Perimeter is: %f", perimeter);
+   int num1, num2, num3, num4;
+   int minimum;
+   printf("Enter your first number");
+   scanf("%d",&num1);
+    printf("Enter your second number");
+    scanf("%d", &num2);
+    printf("Enter your thirst number");
+    scanf("%d", &num3);
+    printf("Enter your fourth number");
+    scanf("%d", &num4);
+    if (num1<num2) (minimum = num1);
+    else minimum = num2;
+    if (num3<minimum) (minimum = num3);
+    if (num4<minimum) (minimum = num4);
+    printf("%d", minimum);
     return 0;
 }
