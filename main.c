1d6f6958e030ef23a10a7a92a9704bff527729de
Hamza Ali <Hamza2.ali@live.uwe.ac.uk>
2026-05-04 01:13:34 +0100

    Week 5 task 1-4

--- main.c
+++ main.c
@@ -1,19 +1,42 @@
 #include <stdio.h>
+#include <math.h>
+//task1
+void print_welcome_message() {
+    printf("ENGINEERING CALCULATOR v1.0"
+           "System Ready. Awaiting input\n");
+}
+//task2
+int add_numbers(int x, int y){
+    return x + y;
+}
+//task3
+float pi = 3.14159;                      //part 1
+double calculate_circumference(double r){
+    return 2 * pi * r;
+}
+double calculate_area(int r){        //  part2
+    return pi * pow(r, 2);
+}
+//task4
+void is_even(int z){
+    if (z % 2 == 0);
+    {
+        printf("1");
+        else
+            printf("0");
+    }
+}
+
 int main() {
-   int num1, num2, num3, num4;
-   int minimum;
-   printf("Enter your first number");
-   scanf("%d",&num1);
-    printf("Enter your second number");
-    scanf("%d", &num2);
-    printf("Enter your thirst number");
-    scanf("%d", &num3);
-    printf("Enter your fourth number");
-    scanf("%d", &num4);
-    if (num1<num2) (minimum = num1);
-    else minimum = num2;
-    if (num3<minimum) (minimum = num3);
-    if (num4<minimum) (minimum = num4);
-    printf("%d", minimum);
+    //task1
+print_welcome_message();
+//task2
+int x, y;
+    printf("Insert your first number");
+    scanf("%d", &x);
+    printf("Insert your second number");
+    scanf("%d", &y);
+    int result = add_numbers(x, y);
+    printf("This is the sum of your numbers: %d", result);
     return 0;
 }
